e8821d5a8289ae345dfaff4eb5afff392414cc37
Kyllian MOSNAT <134300224+kmosnat@users.noreply.github.com>
2024-11-22 07:00:56 +0100

    Update database configuration to use PostgreSQL and add .env to .gitignore

--- Eden/settings.py
+++ Eden/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Eden.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Assurez-vous que ce chemin correspond
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
