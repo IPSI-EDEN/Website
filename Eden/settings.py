cd55744d56222f3a2f4c9931ce4d85e04f71f269
Kyllian MOSNAT <134300224+kmosnat@users.noreply.github.com>
2024-11-22 15:12:33 +0100

    Update database host in settings.py to use external IP address

--- Eden/settings.py
+++ Eden/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'HOST': os.environ.get('DB_HOST', '89.47.50.92'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
