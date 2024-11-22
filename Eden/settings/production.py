4c22f82dd161b04fc81f07eeb478b45b9fc12f6a
Kyllian MOSNAT <134300224+kmosnat@users.noreply.github.com>
2024-11-22 18:11:52 +0100

    Add test, local, and production settings for Django application

---
+++ Eden/settings/production.py
@@ -0,0 +1,24 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['89.47.50.92']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
