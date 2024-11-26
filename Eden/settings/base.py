142368ac08a12294b1dfd434ebc833358b009196
Kyllian MOSNAT <134300224+kmosnat@users.noreply.github.com>
2024-11-26 14:01:38 +0100

    Comment out DisableCSRFForAPI middleware in settings

--- Eden/settings/base.py
+++ Eden/settings/base.py
@@ -24,7 +24,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'Website.middleware.DisableCSRFForAPI',
+    #'Website.middleware.DisableCSRFForAPI',
     'django.middleware.csrf.CsrfViewMiddleware',
     'Website.middleware.SkipLoginForAPI',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
