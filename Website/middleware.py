0988cf73f4b4f34e561b918c1df96c32d8adeb5f
Kyllian MOSNAT <134300224+kmosnat@users.noreply.github.com>
2024-11-26 15:18:15 +0100

    Refactor DisableCSRFForAPI and SkipLoginForAPI middleware to use __call__ method and improve CSRF handling for API requests

--- Website/middleware.py
+++ Website/middleware.py
@@ -3,22 +3,24 @@ from django.utils.deprecation import MiddlewareMixin
 import logging
 logger = logging.getLogger(__name__)
 
-class DisableCSRFForAPI(MiddlewareMixin):
-    def __init__(self, get_response=None):
+class DisableCSRFForAPI:
+    def __init__(self, get_response):
         self.get_response = get_response
-        logger.info("DisableCSRFForAPI chargé")
-        print("DisableCSRFForAPI chargé")
 
-    def process_request(self, request):
+    def __call__(self, request):
+        # Désactiver la vérification CSRF pour les URLs commençant par "/api/"
         if request.path.startswith('/api/'):
             setattr(request, '_dont_enforce_csrf_checks', True)
+        return self.get_response(request)
 
 
-class SkipLoginForAPI(MiddlewareMixin):
-    def __init__(self, get_response=None):
+class SkipLoginForAPI:
+    def __init__(self, get_response):
         self.get_response = get_response
-        print("SkipLoginForAPI chargé")
 
-    def process_request(self, request):
+    def __call__(self, request):
+        # Ignorer l'utilisateur pour les URLs commençant par "/api/"
         if request.path.startswith('/api/'):
             request.user = None
+        return self.get_response(request)
+
